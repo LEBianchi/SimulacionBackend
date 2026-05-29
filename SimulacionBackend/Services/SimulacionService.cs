02665bde8928449f9dab15c040c2e35bda08f74b
LEBianchi <Lisandro.BianchiChaban@alu.frt.utn.edu.ar>
2026-05-29 14:20:53 -0300

    se modifico el calculo del tiempo promedio de tiempo de espera el problema es que se calculaba mal

--- SimulacionBackend/Services/SimulacionService.cs
+++ SimulacionBackend/Services/SimulacionService.cs
@@ -16,6 +16,7 @@ namespace SimulacionBackend.Services
 
         public SimulacionResultDTO EjecutarSimulacion(SimulacionRequestDTO parametros)
         {
+            // Generador pseudoaleatorio basado en el método de Lehmer
             long semilla = DateTime.Now.Ticks % int.MaxValue;
             var congruencial = new CongruencialMixto(semilla, 1664525, 1013904223, (long)Math.Pow(2, 32));
             var dist = new Distribuciones(congruencial);
@@ -24,28 +25,36 @@ namespace SimulacionBackend.Services
             int contador_tablets = 0, contador_celulares = 0;
             int cant_min = 0, cant_rec = 0;
 
-            // Contadores Específicos Nuevos
+            // Contadores Específicos
             int cel_desmantelados = 0, tab_desmanteladas = 0;
             int cel_reacond = 0, tab_reacond = 0;
 
             double TUT = 0, temp_min = 0, temp_borrado = 0, temp_arreglo = 0;
             double total_metales = 0, total_plastico = 0;
-            double total_basura_cruda = 0; // NUEVA VARIABLE: Kilos brutos que llegan en el camión
+            double total_basura_cruda = 0;
 
-            // Variables de colas
+            // Variables de colas y tiempos
             int cola_triage = 0, cola_mineria = 0, cola_reacond = 0;
+            double tiempo_espera_total_minutos = 0; // Acumulador de espera real (Wq)
+
+            // Relojes de disponibilidad de operarios (cuándo se desocupa cada área)
+            double reloj_triage = 0;
+            double reloj_mineria = 0;
+            double reloj_reacond = 0;
 
             double maxMinutosTriage = parametros.HorasJornada * 60.0 * parametros.EmpleadosTriage;
             double maxMinutosMineria = parametros.HorasJornada * 60.0 * parametros.EmpleadosDesmantelamiento;
             double maxMinutosReacond = parametros.HorasJornada * 60.0 * parametros.EmpleadosReaciclaje;
 
             int lotesDelDia = dist.Poisson(4);
+            double tiempo_llegada_acumulado = 0;
 
             for (int j = 1; j <= lotesDelDia; j++)
             {
-                double kgLote = dist.Normal(120, 15);
+                // Simulamos la llegada de los camiones durante la jornada
+                tiempo_llegada_acumulado += dist.Uniforme(30, 120);
 
-                // ACÁ SUMAMOS EL PESO DEL CAMIÓN A LA BASURA CRUDA
+                double kgLote = dist.Normal(120, 15);
                 total_basura_cruda += kgLote;
 
                 double kg_telTabl = kgLote * 0.08;
@@ -54,9 +63,8 @@ namespace SimulacionBackend.Services
                 while (peso_acumulado < kg_telTabl)
                 {
                     double u_tipo = congruencial.obtenerSiguiente();
-                    bool isCelular = u_tipo <= 0.7; // Variable para saber qué es
+                    bool isCelular = u_tipo <= 0.7;
 
-                    // Contamos los que ingresan a la planta NI BIEN ENTRAN
                     if (isCelular) contador_celulares++;
                     else contador_tablets++;
 
@@ -65,50 +73,89 @@ namespace SimulacionBackend.Services
 
                     peso_acumulado += peso_actual;
 
+                    // --- LÓGICA ESTOCÁSTICA DE EVENTOS DISCRETOS ---
+
+                    // 1. TRIAGE
                     double t_triage = dist.Uniforme(15, 20);
-                    if (TUT + t_triage > maxMinutosTriage)
+
+                    double inicio_triage = Math.Max(tiempo_llegada_acumulado, reloj_triage);
+                    double espera_triage = inicio_triage - tiempo_llegada_acumulado;
+
+                    if (inicio_triage + t_triage > maxMinutosTriage)
                     {
                         cola_triage++;
+                        tiempo_espera_total_minutos += (maxMinutosTriage - tiempo_llegada_acumulado);
                         continue;
                     }
 
+                    tiempo_espera_total_minutos += espera_triage;
+                    reloj_triage = inicio_triage + t_triage;
                     TUT += t_triage;
 
+                    // 2. DESTINO
                     double u_destino = congruencial.obtenerSiguiente();
+                    double tiempo_salida_triage = reloj_triage;
+
                     if (u_destino <= 0.1)
                     {
-                        // Basura directa, no suma a minería ni reacondicionamiento
+                        // Basura directa (rechazo)
                     }
                     else if (u_destino <= 0.25)
                     {
-                        bool exito = IntentarReacondicionamiento(congruencial, dist, maxMinutosReacond, ref cant_rec, ref temp_borrado, ref temp_arreglo);
-                        if (!exito)
+                        // REACONDICIONAMIENTO
+                        double t_borrado = dist.Uniforme(30, 45);
+                        double t_arreglo = congruencial.obtenerSiguiente() <= 0.65 ? dist.Exponencial(90) : dist.Exponencial(180);
+                        double t_total_reacond = t_borrado + t_arreglo;
+
+                        double inicio_reacond = Math.Max(tiempo_salida_triage, reloj_reacond);
+                        double espera_reacond = inicio_reacond - tiempo_salida_triage;
+
+                        if (inicio_reacond + t_total_reacond > maxMinutosReacond)
                         {
                             cola_reacond++;
+                            tiempo_espera_total_minutos += (maxMinutosReacond - tiempo_salida_triage);
                         }
                         else
                         {
-                            // Si tuvo éxito, sumamos al contador específico
+                            tiempo_espera_total_minutos += espera_reacond;
+                            reloj_reacond = inicio_reacond + t_total_reacond;
+                            cant_rec++;
+                            temp_borrado += t_borrado;
+                            temp_arreglo += t_arreglo;
                             if (isCelular) cel_reacond++; else tab_reacond++;
                         }
                     }
                     else
                     {
-                        bool exito = IntentarMineria(dist, peso_actual, maxMinutosMineria, ref cant_min, ref temp_min, ref total_metales, ref total_plastico);
-                        if (!exito)
+                        // MINERÍA (Desmantelamiento)
+                        double t_min = dist.Uniforme(25, 40);
+
+                        double inicio_mineria = Math.Max(tiempo_salida_triage, reloj_mineria);
+                        double espera_mineria = inicio_mineria - tiempo_salida_triage;
+
+                        if (inicio_mineria + t_min > maxMinutosMineria)
                         {
                             cola_mineria++;
+                            tiempo_espera_total_minutos += (maxMinutosMineria - tiempo_salida_triage);
                         }
                         else
                         {
-                            // Si tuvo éxito, sumamos al contador específico
+                            tiempo_espera_total_minutos += espera_mineria;
+                            reloj_mineria = inicio_mineria + t_min;
+                            cant_min++;
+                            temp_min += t_min;
+                            total_metales += (peso_actual * 0.35);
+                            total_plastico += (peso_actual * 0.40);
                             if (isCelular) cel_desmantelados++; else tab_desmanteladas++;
                         }
                     }
                 }
             }
 
-            double tiempoTotalHoras = (TUT + temp_min + temp_borrado + temp_arreglo) / 60.0;
+            // Transformamos el acumulador de minutos en horas promedio
+            double tiempoTotalHoras = tiempo_espera_total_minutos / 60.0;
+
+            // Cálculos de eficiencia
             double eficTriage = maxMinutosTriage > 0 ? (TUT / maxMinutosTriage) * 100 : 0;
             double eficMineria = maxMinutosMineria > 0 ? (temp_min / maxMinutosMineria) * 100 : 0;
             double eficReacond = maxMinutosReacond > 0 ? ((temp_borrado + temp_arreglo) / maxMinutosReacond) * 100 : 0;
@@ -127,11 +174,7 @@ namespace SimulacionBackend.Services
                 EquiposEnColaTriage = cola_triage,
                 EquiposEnColaDesmantelamiento = cola_mineria,
                 EquiposEnColaReacondicionamiento = cola_reacond,
-
-                // Mapeo del nuevo campo de basura cruda
                 KilosBasuraFisicaTotal = Math.Round(total_basura_cruda, 2),
-
-                // Mapeo de los campos a la BD
                 CelularesIngresados = contador_celulares,
                 TabletsIngresadas = contador_tablets,
                 CelularesDesmantelados = cel_desmantelados,
@@ -157,11 +200,7 @@ namespace SimulacionBackend.Services
                 EquiposEnColaTriage = registro.EquiposEnColaTriage,
                 EquiposEnColaDesmantelamiento = registro.EquiposEnColaDesmantelamiento,
                 EquiposEnColaReacondicionamiento = registro.EquiposEnColaReacondicionamiento,
-
-                // Mapeo del nuevo campo a React
                 KilosBasuraFisicaTotal = registro.KilosBasuraFisicaTotal,
-
-                // Mapeo de los campos a React
                 CelularesIngresados = registro.CelularesIngresados,
                 TabletsIngresadas = registro.TabletsIngresadas,
                 CelularesDesmantelados = registro.CelularesDesmantelados,
@@ -169,31 +208,6 @@ namespace SimulacionBackend.Services
                 CelularesReacondicionados = registro.CelularesReacondicionados,
                 TabletsReacondicionadas = registro.TabletsReacondicionadas
             };
-        }
-
-        private bool IntentarMineria(Distribuciones dist, double peso_actual, double maxMinutos, ref int cant_min, ref double temp_min, ref double total_metales, ref double total_plastico)
-        {
-            double t_min = dist.Uniforme(25, 40);
-            if (temp_min + t_min > maxMinutos) return false;
-
-            cant_min++;
-            temp_min += t_min;
-            total_metales += (peso_actual * 0.35);
-            total_plastico += (peso_actual * 0.40);
-            return true;
-        }
-
-        private bool IntentarReacondicionamiento(CongruencialMixto congruencial, Distribuciones dist, double maxMinutos, ref int cant_rec, ref double temp_borrado, ref double temp_arreglo)
-        {
-            double t_borrado = dist.Uniforme(30, 45);
-            double t_arreglo = congruencial.obtenerSiguiente() <= 0.65 ? dist.Exponencial(90) : dist.Exponencial(180);
-
-            if (temp_borrado + temp_arreglo + t_borrado + t_arreglo > maxMinutos) return false;
-
-            cant_rec++;
-            temp_borrado += t_borrado;
-            temp_arreglo += t_arreglo;
-            return true;
         }
     }
 }
